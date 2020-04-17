98b4dc714a3f90244b64e14e886b732d07e0379b
62reigot <banana0602877@gmail.com>
2020-04-17 23:09:21 +0900

    不要なインポート文を削除しました。 glideライブラリを適用しました。 変数名を命名規則に従って変更しました。 アニメーションクラスの代わりにGif動画を使うよう変更しました。 MagicNumberManagerクラスを作成し、ここにほぼ全ての定数を保存しました。 onCreate内のコードをいくつかのメソッドに分割しました。 BGMとSEの音量を下げました。 isSecondJumping変数の代わりにisFirstJumping変数を作りました。 playSEクラスを作成し、SEの呼び出しを簡略化しました。 コード全体に目を通し、必要な個所にコメントを加えました。
    次は障害物を用意して右から左へ流すプログラムと、当たり判定のプログラムを加える予定です。

--- app/src/main/java/com/reigot/gameapppractice_animation/MainActivity.java
+++ app/src/main/java/com/reigot/gameapppractice_animation/MainActivity.java
@@ -1,31 +1,28 @@
 package com.reigot.gameapppractice_animation;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.solver.widgets.Guideline;
 
 import android.media.MediaPlayer;
 import android.media.SoundPool;
-import android.net.Uri;
 import android.os.Bundle;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
-import android.util.DisplayMetrics;
 import android.util.SparseIntArray;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     //画面部品
-    private ImageView _zoukinGirl;
-    private TextView _tvZoukinY;
+    private ImageView ivClothGirl;
+    private TextView tvClothGirlY;
 
     //サウンド
     private MediaPlayer mediaPlayer;
@@ -34,39 +31,52 @@ public class MainActivity extends AppCompatActivity {
 
     //タイマー
     private Timer jumpTimer;
-    private Handler mHandler = new Handler();
+    private Handler jumpHandler = new Handler();
 
     //デバイス情報
     private float scale;
-    private int screenWidth;
-    private int screenHeight;
 
     //雑巾少女の配置
-    private float _zoukinGirlY;
-    private float _zoukinGirlYPrev;
-    private float _zoukinGirlYDefault;
+    private float clothGirlY;
+    private float clothGirlPrev;
+    private float ClothGirlDefaultY;
 
     //ジャンプ処理
     private boolean isJumping;
-    private boolean isSecondJumping;
+    private boolean isFirstJumping;
     private int delay;
-    private float jumpPower;
+    private float jumpingPower;
 
-    //アニメーション
-    private AnimationDrawable animation;
+    public static class MagicNumberManager {
+        //BGMの音量
+        static final float MAIN_BGM_VOLUME = 0.8f;
 
-
+        //ジャンプ関連の定数
+        static final float JUMP_SE_VOLUME = 0.2f;
+        static final float JUMP_SE_RATE = 1.0f;
+        static final int FIRST_JUMPING_DELAY = 0;
+        static final int SECOND_JUMPING_DELAY = 30;
+        static final int JUMP_TIMER_TASK_PERIOD = 9;
+        static final float FIRST_JUMPING_POWER_BASE = 15.0f;  //judgeJumpingSituationにて "4.0f * scale" に足す数。
+        static final float SECOND_JUMPING_POWER_BASE = 16.0f; //judgeJumpingSituationにて "4.0f * scale" に足す数。
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //画面サイズによってジャンプ力を調整するための変数を用意。
         scale = getResources().getDisplayMetrics().density;
-        DisplayMetrics dMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dMetrics);
-        screenWidth = dMetrics.widthPixels;
-        screenHeight = dMetrics.heightPixels;
+
+        //ステータスバーを非表示に変更。
+        hideStatusBar();
+
+        //画面部品の取得。
+        findViewContent();
+
+        //Gif動画の適用。
+        loadGifAnimation();
 
         //BGMの再生。
         playBGM(R.raw.main);
@@ -75,20 +85,37 @@ public class MainActivity extends AppCompatActivity {
         //入力したパスはsoundIdMapのkeyとして登録される。（可変長引数）
         prepareSE(R.raw.jump);
 
+        //TO DO: 障害物（机）を準備。
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //タッチされたらジャンプ処理へ進む。
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            judgeJumpingSituation();
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void hideStatusBar() {
         View decorView = getWindow().getDecorView();
-        // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+    }
 
-        _tvZoukinY = findViewById(R.id.tvMain_zoukinY);
-        _zoukinGirl = findViewById(R.id.zoukinGirl);
+    public void findViewContent() {
+        tvClothGirlY = findViewById(R.id.tvMain_zoukinY);
+        ivClothGirl = findViewById(R.id.zoukinGirl);
+    }
 
-        animationPrepare();
+    public void loadGifAnimation() {
+        Glide.with(this).load(R.raw.zoukin_girl_run).into(ivClothGirl);
     }
 
     public void playBGM(int bgmFilePath) {
         mediaPlayer = MediaPlayer.create(this, bgmFilePath);
+        mediaPlayer.setVolume(MagicNumberManager.MAIN_BGM_VOLUME, MagicNumberManager.MAIN_BGM_VOLUME);
+        mediaPlayer.setLooping(true);
         mediaPlayer.start();
     }
 
@@ -105,73 +132,65 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void animationPrepare() {
-        _zoukinGirl.setBackgroundResource(R.drawable.zoukin_girl_list);
-        animation = (AnimationDrawable) _zoukinGirl.getBackground();
-        animation.start();
+    public void playSE(int seFilePath, float volume, float rate) {
+        soundPool.play(soundIdMap.get(seFilePath), volume, volume, 0, 0, rate);
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN) {
-            jumpJudge();
-        }
-
-        return super.onTouchEvent(event);
-    }
-
-    public void jumpJudge() {
-
-
-
+    public void judgeJumpingSituation() {
+        final float FIRST_JUMPING_POWER = 4.0f * scale + MagicNumberManager.FIRST_JUMPING_POWER_BASE;
+        final float SECOND_JUMPING_POWER = 4.0f * scale + MagicNumberManager.SECOND_JUMPING_POWER_BASE;
 
         if (!(isJumping)) {
-            soundPool.play(soundIdMap.get(R.raw.jump), 1.0f, 1.0f, 0, 0, 1.0f);
-            _zoukinGirlYDefault = _zoukinGirlY = _zoukinGirl.getY();
-            delay = 0;
-            jumpPower = 4.0f * scale + 15.0f;
-            jumpAction();
-        } else if (!(isSecondJumping)) {
-            soundPool.play(soundIdMap.get(R.raw.jump), 1.0f, 1.0f, 0, 0, 1.0f);
-            isSecondJumping = true;
-            delay = 30;
-            jumpPower = 4.0f * scale + 16.0f;
+            //ジャンプ中でなければ1段目のジャンプをする。
+            isJumping = true;
+            isFirstJumping = true;
+            playSE(R.raw.jump, MagicNumberManager.JUMP_SE_VOLUME, MagicNumberManager.JUMP_SE_RATE);
+            ClothGirlDefaultY = clothGirlY = ivClothGirl.getY();
+            delay = MagicNumberManager.FIRST_JUMPING_DELAY;
+            jumpingPower = FIRST_JUMPING_POWER;
+            startJumping();
+        } else if (isFirstJumping) {
+            //1段目のジャンプ中ならば2段目のジャンプをする。
+            isFirstJumping = false;
+            playSE(R.raw.jump, MagicNumberManager.JUMP_SE_VOLUME, MagicNumberManager.JUMP_SE_RATE);
+            delay = MagicNumberManager.SECOND_JUMPING_DELAY;
+            jumpingPower = SECOND_JUMPING_POWER;
             jumpTimer.cancel();
-            jumpAction();
+            startJumping();
         }
-
     }
 
-    public void jumpAction() {
-        _zoukinGirl.setBackgroundResource(R.drawable.zoukin_girl_jump0);
-        _zoukinGirlYPrev = _zoukinGirlY;
-        _zoukinGirlY -= jumpPower;
-        _zoukinGirl.setY(_zoukinGirlY);
+    public void startJumping() {
+        Glide.with(this).load(R.drawable.zoukin_girl_jump0).into(ivClothGirl);
+        clothGirlPrev = clothGirlY;
+        clothGirlY -= jumpingPower;
+        ivClothGirl.setY(clothGirlY);
         jumpTimer = new Timer();
-        TimerTask jumpTimerTask = new JumpTimerTask();
-        jumpTimer.schedule(jumpTimerTask, delay, 9);
+        TimerTask jumpTimerTask = new JumpingTimerTask();
+        jumpTimer.schedule(jumpTimerTask, delay, MagicNumberManager.JUMP_TIMER_TASK_PERIOD);
     }
 
-    public class JumpTimerTask extends TimerTask {
+    public class JumpingTimerTask extends TimerTask {
         @Override
         public void run() {
-            mHandler.post(new Runnable() {
+            jumpHandler.post(new Runnable() {
                 @Override
                 public void run() {
+                    //自然なジャンプをするための構文
+                    tvClothGirlY.setText(String.valueOf(clothGirlY));
+                    float y_temp = clothGirlY;
+                    clothGirlY += (clothGirlY - clothGirlPrev) + 1;
+                    clothGirlPrev = y_temp;
+                    ivClothGirl.setY(clothGirlY);
 
-                    _tvZoukinY.setText(String.valueOf(_zoukinGirlY));
-                    isJumping = true;
-                    float y_temp = _zoukinGirlY;
-                    _zoukinGirlY += (_zoukinGirlY - _zoukinGirlYPrev) + 1;
-                    _zoukinGirlYPrev = y_temp;
-                    _zoukinGirl.setY(_zoukinGirlY);
 
-                    if(_zoukinGirlY > _zoukinGirlYDefault) {
-                        _zoukinGirlY = _zoukinGirlYDefault;
-                        _zoukinGirl.setY(_zoukinGirlY);
+                    if(clothGirlY > ClothGirlDefaultY) {
+                        //キャラクターが元の高さに戻ったらジャンプアクションを終了する。
+                        clothGirlY = ClothGirlDefaultY;
+                        ivClothGirl.setY(clothGirlY);
                         isJumping = false;
-                        isSecondJumping = false;
-                        animationPrepare();
+                        isFirstJumping = false;
+                        loadGifAnimation();
                         jumpTimer.cancel();
                     }
                 }
